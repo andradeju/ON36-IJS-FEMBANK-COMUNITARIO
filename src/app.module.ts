9c6e2d563ae30b0f94e01da04d0822683dfaa9f4
andradeju <julliana.andrade08@gmail.com>
2024-07-24 14:05:00 -0300

    :wrench: chore: NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ContasController } from './contas/contas.controller';
 import { ContasService } from './contas/contas.service';
 import { ContasModule } from './contas/contas.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { GerentesModule } from './gerentes/gerentes.module';
 
 @Module({
-  imports: [ContasModule],
+  imports: [ContasModule, ClientesModule, GerentesModule],
   controllers: [AppController, ContasController],
   providers: [AppService, ContasService],
 })
